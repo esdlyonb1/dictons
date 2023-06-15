4484795808426de24b740d20cf969a1654d57f33
esdb1 <pierre.lacognata@mail-esd.com>
2023-06-15 15:55:28 +0200

    image upload

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
---
+++ src/Controller/ImageController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Image;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Vich\UploaderBundle\Templating\Helper\UploaderHelper;
+
+#[Route( "/api")]
+class ImageController extends AbstractController
+{
+    #[Route('/imageupload', name: 'app_image', methods: ["POST"])]
+    public function upload(UploaderHelper $helper,Request $request, EntityManagerInterface $manager): Response
+    {
+        $image = new Image();
+
+        $file = $request->files->get('monImage');
+
+        $image->setImageFile($file);
+
+        $manager->persist($image);
+        $manager->flush();
+
+
+        $response = [
+            "message"=>"bravo pour ton upload",
+            "idImage"=>$image->getId(),
+            "image"=>"https://localhost:8000".$helper->asset($image)
+
+        ];
+
+
+
+
+        return $this->json($response,200);
+    }
+}
